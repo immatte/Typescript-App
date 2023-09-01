baf33118727a36e95a70a5bc23ef7c55419975b9
immatte <matte_isabelle@yahoo.fr>
2023-09-01 15:32:21 +0200

    Update

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -6,3 +6,9 @@ export const Child = ({ color }: ChildProps) => {
     return <div>{color}</div>;
 };
 
+
+export const ChildAsFC: React.FC<ChildProps> = ({ color }) => {
+    return <div>{color}</div>;
+};
+
+ChildAsFC.displayName
