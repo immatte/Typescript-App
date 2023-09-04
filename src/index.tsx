d5f812c3d9bf684bb5995c787d77e9ba46b9a98c
immatte <matte_isabelle@yahoo.fr>
2023-09-04 09:49:11 +0200

    EventComponent

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import ReactDOM from "react-dom";
 import GuestList from "./state/Guest.list";
 import UserSearch from "./state/UserSearch";
+import EventComponent from "./events/EventComponent";
 
 const App = () => {
 return (<div>
     <h1>Hi there!</h1>
     <GuestList/>
     <UserSearch/>
+    <EventComponent/>
 </div>
 );
 
